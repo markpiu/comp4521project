48b73a0db26da06ddc6720a26e12a8b8f5daf1a2
Mark Piu <>
2023-05-09 03:18:13 +0800

    Added password reset

--- app/src/main/java/comp4521/group_s/Login.java
+++ app/src/main/java/comp4521/group_s/Login.java
@@ -9,10 +9,15 @@ import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
@@ -26,7 +31,12 @@ public class Login extends AppCompatActivity {
     Button buttonLogin;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
-    TextView RegNowTextView;
+    TextView RegNowTextView,ForgotPasswordTextView;
+    ImageView GoogleLoginImageView;
+
+    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build();
 
     @Override
     public void onStart() {
@@ -50,6 +60,9 @@ public class Login extends AppCompatActivity {
         buttonLogin = findViewById(R.id.btnLogin);
         progressBar = findViewById(R.id.progressBar);
         RegNowTextView = findViewById(R.id.registerNow);
+        GoogleLoginImageView = findViewById(R.id.image_googlesignin);
+        ForgotPasswordTextView = findViewById(R.id.ForgotPassword);
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
@@ -101,8 +114,40 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        ForgotPasswordTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email;
+                email = String.valueOf(editTextEmail.getText());
+
+                if(TextUtils.isEmpty(email)|| !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    Toast.makeText(Login.this, "Enter your email to reset your password",Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                    return;
+                }
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this, "We have sent you an email to reset your password!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Login.this, "Invalid Email or Error Occurs", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        });
 
 
+        GoogleLoginImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Google login is not available now",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
