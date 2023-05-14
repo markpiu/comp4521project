879d9570281fbaa8dce8168a2b27d5e8fdb571fc
Mark Piu <>
2023-05-15 04:44:12 +0800

    minor bug fix

--- app/src/main/java/comp4521/group_s/ArticleWebsite.java
+++ app/src/main/java/comp4521/group_s/ArticleWebsite.java
@@ -38,7 +38,6 @@ public class ArticleWebsite extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_article_website);
 
 
