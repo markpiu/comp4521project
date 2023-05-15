1959c5d4a3870ef60b4542ce4428f04062d9f089
MattMa123 <112682932+MattMa123@users.noreply.github.com>
2023-05-15 09:49:42 +0800

    Fixed nutrient tracker, changed button layout

--- app/src/main/java/comp4521/group_s/NutrientTrackingActivity.java
+++ app/src/main/java/comp4521/group_s/NutrientTrackingActivity.java
@@ -1,18 +1,15 @@
 package comp4521.group_s;
 
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -87,13 +84,10 @@ public class NutrientTrackingActivity extends AppCompatActivity {
             finish();
         }
 
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        toolbar.setNavigationOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
         });
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -154,6 +148,7 @@ public class NutrientTrackingActivity extends AppCompatActivity {
 
             } while (cursor.moveToNext());
         }
+        Log.i("DATA", data.toString());
         LineDataSet lineDataSet;
         averageConsumption = 0;
         if (data.size() < 7) {
@@ -237,7 +232,7 @@ public class NutrientTrackingActivity extends AppCompatActivity {
             Food lunchChosen = getFood(lunchSpinner.getSelectedItem().toString(), lunches);
             Food dinnerChosen = getFood(dinnerSpinner.getSelectedItem().toString(), dinners);
 
-            String[] inputTypes = new String[]{"Calories", "Carbohydrates", "Protein", "Vitamins", "Minerals"};
+            String[] inputTypes = new String[]{"Calories (kcal)", "Carbohydrates (g)", "Protein (g)", "Vitamins (mg)", "Minerals (mg)"};
 
             for (int i = 0; i < 5; i++) {
                 current_ID = Long.valueOf(dbManager.getSize()).intValue() + 1;
